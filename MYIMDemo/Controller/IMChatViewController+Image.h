eb3430a8bc7c3e85b932e5390de7ea5088adbffd
pterXX <728102384@qq.com>
2019-03-04 09:54:53 +0800

    添加文件

---
+++ MYIMDemo/Controller/IMChatViewController+Image.h
@@ -0,0 +1,23 @@
+//
+//  IMChatViewController+Image.h
+//  KXiniuCloud
+//
+//  Created by eims on 2018/5/11.
+//  Copyright © 2018年 EIMS. All rights reserved.
+//
+
+#import "IMChatViewController.h"
+
+@interface IMChatViewController (Image)
+
+/**
+ 从相册选择照片
+ */
+- (void)selectPhoto;
+
+/**
+ 拍照或者录短视频
+ */
+- (void)takePhoto;
+
+@end
---
+++ MYIMDemo/Model/IMMailAccountInfoModel.m
@@ -0,0 +1,13 @@
+//
+//  IMMailAccountInfoModel.m
+//  KXiniuCloud
+//
+//  Created by RPK on 2017/8/14.
+//  Copyright © 2017年 EIMS. All rights reserved.
+//
+
+#import "IMMailAccountInfoModel.h"
+
+@implementation IMMailAccountInfoModel
+
+@end
